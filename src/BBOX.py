78b6c44747e12ac382f74a357b931a1b26c0abb0
Artur C. Fabrício <arturfabricio@gmail.com>
2022-11-22 14:53:59 +0100

    working dataset

--- src/BBOX.py
+++ src/BBOX.py
@@ -7,7 +7,6 @@ import numpy as np
 import matplotlib.patches as patches
 import skimage.io
 
-
 class BBOX():
     def __init__(self, min_x: int, min_y: int, width: int, height: int):
         self.arr = np.array([min_x, min_y, min_x+width, min_y+height])
@@ -16,11 +15,11 @@ class BBOX():
         return "BBOX: % s" % (self.arr)
 
 
-def generate(s, n, increment, image_path) -> list[BBOX]:
+def generate(s, n, increment, im_shape) -> list[BBOX]:
     scaling = 1.0/s+2
     bbox_final = []
 
-    x_list, y_list, _ = get_referencepoints(image_path, s)
+    x_list, y_list, _ = get_referencepoints(im_shape, s)
 
     for x in x_list:
         for y in y_list:
@@ -34,8 +33,13 @@ def generate(s, n, increment, image_path) -> list[BBOX]:
     return bbox_final
 
 
-def display(BBOXs: list[BBOX], image_path, s):
-    im = Image.open(image_path)
+def display(BBOXs: list[BBOX], image_path, s, new_size, ratio):
+    # im = Image.open(image_path)
+
+    im = transformsXY_im(image_path,new_size)
+
+
+
     fig, ax = plt.subplots()
     ax.imshow(im)
 
@@ -50,9 +54,15 @@ def display(BBOXs: list[BBOX], image_path, s):
     plt.show()
 
 
-def get_referencepoints(img_path, s):
-    im = cv2.imread(str(img_path))
-    width, height, *_ = im.shape
+def get_referencepoints(im_shape, s):
+    #im = cv2.imread(str(img_path))
+
+    width = 128
+    height = 128
+
+    print('Width: ', width)
+    print('Heigth: ', height)
+
     width_incr = int(width//s)
     height_incr = int(height//s)
     x = []
@@ -68,3 +78,21 @@ def get_referencepoints(img_path, s):
 def get_random_color():
     h, s, l = random.uniform(0, 1), 1, 0.5
     return colorsys.hls_to_rgb(h, l, s)
+
+
+def transformsXY(path, bb, new_size, ratio):
+    x = cv2.imread(str(path)).astype(np.float32)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)/255
+    x = cv2.resize(x,(new_size,new_size) ) 
+    bb[0] = int(bb[0]/ratio)
+    bb[1] = int(bb[1]/ratio)
+    bb[2] = int(bb[2]/ratio)
+    bb[3] = int(bb[3]/ratio)
+    return x, bb 
+
+
+def transformsXY_im(path, new_size):
+    x = cv2.imread(str(path)).astype(np.float32)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)/255
+    x = cv2.resize(x,(new_size,new_size) ) 
+    return x
